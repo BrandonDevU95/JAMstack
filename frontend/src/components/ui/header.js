5fa4bcaf48520f45475606a0a15be342664e296e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-16 21:44:22 -0500

    46. Encabezado P3

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -6,7 +6,11 @@ import {
   Button,
   Tabs,
   Tab,
+  IconButton,
 } from "@material-ui/core"
+import search from "../../images/search.svg"
+import cart from "../../images/cart.svg"
+import account from "../../images/account-header.svg"
 
 export default function Header() {
   return (
@@ -15,6 +19,21 @@ export default function Header() {
         <Button>
           <Typography variant="h1">Var X</Typography>
         </Button>
+        <Tabs>
+          <Tab label="Hats" />
+          <Tab label="Hoodies" />
+          <Tab label="Shirts" />
+          <Tab label="Contact Us" />
+        </Tabs>
+        <IconButton>
+          <img src={search} alt="search" />
+        </IconButton>
+        <IconButton>
+          <img src={cart} alt="cart" />
+        </IconButton>
+        <IconButton>
+          <img src={account} alt="account" />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )

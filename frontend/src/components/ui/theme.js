468ff7cbb33fa6f60e54fa58d19adfa123844acc
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-16 20:36:10 -0500

    43. Tema personalizado P3

---
+++ frontend/src/components/ui/theme.js
@@ -0,0 +1,36 @@
+import { createMuiTheme } from "@material-ui/core/styles"
+
+const green = "#99B898"
+const darkGreen = "#708670"
+const tan = "#FECEA8"
+const lightRed = "#FF847C"
+const red = "#E84A5F"
+offBlack = "#2A363B"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green,
+    },
+    secondary: {
+      main: darkGreen,
+    },
+    common: {
+      tan,
+      lightRed,
+      red,
+      offBlack,
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "4.5rem",
+      fontFamily: "Philosopher",
+      fontStyle: "italic",
+      fontWeight: 700,
+    },
+  },
+  overrides: {},
+})
+
+export default theme

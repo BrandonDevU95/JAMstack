f6dd8e7f7cafe2853efacc099f50cfd2ec605ba0
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-17 14:28:05 -0500

    48. Personalización de componentes P1

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -8,18 +8,26 @@ import {
   Tab,
   IconButton,
 } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 import search from "../../images/search.svg"
 import cart from "../../images/cart.svg"
 import account from "../../images/account-header.svg"
 
+const useStyles = makeStyles(theme => ({
+  coloredIndicator: {
+    backgroundColor: "red",
+  },
+}))
+
 export default function Header() {
+  const classes = useStyles()
   return (
     <AppBar elevation={0} color="transparent">
       <Toolbar>
         <Button>
           <Typography variant="h1">Var X</Typography>
         </Button>
-        <Tabs>
+        <Tabs value={0} classes={{ indicator: classes.coloredIndicator }}>
           <Tab label="Hats" />
           <Tab label="Hoodies" />
           <Tab label="Shirts" />

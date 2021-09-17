fccac5c2127409f3132754286a94f420e2069a5f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-16 20:47:54 -0500

    43. Tema personalizado P4

--- frontend/src/components/ui/theme.js
+++ frontend/src/components/ui/theme.js
@@ -5,7 +5,9 @@ const darkGreen = "#708670"
 const tan = "#FECEA8"
 const lightRed = "#FF847C"
 const red = "#E84A5F"
-offBlack = "#2A363B"
+const offBlack = "#2A363B"
+const grey = "#747474"
+const white = "#FFFFFF"
 
 const theme = createMuiTheme({
   palette: {
@@ -28,6 +30,38 @@ const theme = createMuiTheme({
       fontFamily: "Philosopher",
       fontStyle: "italic",
       fontWeight: 700,
+      color: green,
+    },
+    h2: {
+      fontFamily: "Montserrat",
+      fontSize: "3rem",
+      fontWeight: 500,
+      color: white,
+    },
+    h3: {
+      fontSize: "2rem",
+      fontFamily: "Montserrat",
+      fontWeight: 300,
+      color: green,
+    },
+    h4: {
+      fontFamily: "Philosopher",
+      fontSize: "3rem",
+      fontStyle: "italic",
+      fontWeight: 700,
+      color: white,
+    },
+    h5: {
+      fontFamily: "Philosopher",
+      fontSize: "2rem",
+      fontStyle: "italic",
+      fontWeight: 700,
+      color: white,
+    },
+    body1: {
+      fontFamily: "Montserrat",
+      fontSize: "1.5rem",
+      color: grey,
     },
   },
   overrides: {},

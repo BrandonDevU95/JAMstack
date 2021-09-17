6e80a127f9b389a1ca3995c201be28584f2b137c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-17 14:06:59 -0500

    47. Funetes

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-material-ui`,
     {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Philosopher:i7:latin", "Montserrat:n5,n4,n3:latin"],
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `http://localhost:1337`,

6acaee5307d93c0f5dc03096718b03767a6b6e0a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-09-18 20:20:06 -0500

    54. Uso del diseño responsivo

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -7,12 +7,19 @@ import {
   Tabs,
   Tab,
   IconButton,
+  useMediaQuery,
+  Hidden,
+  makeStyles,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemText,
 } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
 import search from "../../images/search.svg"
 import cart from "../../images/cart.svg"
 import account from "../../images/account-header.svg"
 
+//Estilos personalizados
 const useStyles = makeStyles(theme => ({
   coloredIndicator: {
     backgroundColor: "red",
@@ -26,12 +33,44 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+//Las categories vienen del layout y se agrega al final el Contact us con el spread operator
 export default function Header({ categories }) {
   const routes = [
     ...categories,
     { node: { name: "Contact Us", strapiId: "contact" } },
   ]
+
+  //Implmentacion de estilos
   const classes = useStyles()
+  //Para saber si es un dispositivo movil md
+  const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
+
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent)
+
+  //Renderiza las tabs en una constante
+  const tabs = (
+    <Tabs
+      value={0}
+      classes={{ indicator: classes.coloredIndicator, root: classes.tabs }}
+    >
+      {routes.map(route => (
+        <Tab label={route.node.name} key={route.node.strapiId} />
+      ))}
+    </Tabs>
+  )
+
+  const drawer = (
+    <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS}>
+      <List disablePadding>
+        <ListItem>
+          <ListItemText primary="Home" />
+        </ListItem>
+      </List>
+    </SwipeableDrawer>
+  )
+
   return (
     <AppBar elevation={0} color="transparent">
       <Toolbar>
@@ -40,23 +79,20 @@ export default function Header({ categories }) {
             <span className={classes.logoText}>VAR</span> X
           </Typography>
         </Button>
-        <Tabs
-          value={0}
-          classes={{ indicator: classes.coloredIndicator, root: classes.tabs }}
-        >
-          {routes.map(route => (
-            <Tab label={route.node.name} key={route.node.strapiId} />
-          ))}
-        </Tabs>
+        {/* Se renderizan las tabs si la pantalla es superior a MD */}
+        {matchesMD ? null : tabs}
         <IconButton>
           <img src={search} alt="search" />
         </IconButton>
         <IconButton>
           <img src={cart} alt="cart" />
         </IconButton>
-        <IconButton>
-          <img src={account} alt="account" />
-        </IconButton>
+        {/* Componente Hidden para que se muestre solo en pantallas grandes */}
+        <Hidden mdDown>
+          <IconButton>
+            <img src={account} alt="account" />
+          </IconButton>
+        </Hidden>
       </Toolbar>
     </AppBar>
   )

a3ea59f6d02178e417f348bfa7abe2a9463e853a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-13 22:02:16 -0500

    Configurando base de datos

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,10 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
+        host: env("DATABASE_HOST"),
+        srv: env.bool("DATABASE_SRV"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
-        ssl: true,
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL"),
       },
     },
   },

63841acf0e5332a27d1bd5fc59b286ed2720cdc9
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-27 23:35:20 -0500

    78. Botones de marketing P2

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from "../components/ui/layout"
 import HeroBlock from "../components/home/HeroBlock"
 import PromotionalProducts from "../components/home/PromotionalProducts"
 import FeaturedProductions from "../components/home/FeaturedProductions"
+import MarketingButtons from "../components/home/MarketingButtons"
 
 const IndexPage = () => (
   <Layout>
     <HeroBlock />
     <PromotionalProducts />
     <FeaturedProductions />
+    <MarketingButtons />
   </Layout>
 )
 

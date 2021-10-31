31b0e2d95e46e0bbad82f248acf4865c1d2c8e38
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-10-30 23:51:20 -0500

    88. Estilos de la pagina de contacto p2

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import clsx from "clsx"
 import { Typography, Grid, Button } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import Layout from "../components/ui/layout"
@@ -20,15 +21,38 @@ const useStyles = makeStyles(theme => ({
   formWrapper: {
     height: "100%",
   },
-  titleContainer: {
+  blockContainer: {
     backgroundColor: theme.palette.secondary.main,
-    height: "5rem",
-    width: "20rem",
+    height: "8rem",
+    width: "40rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleContainer: {
+    marginTop: "-4rem",
   },
   buttonContainer: {
-    backgroundColor: theme.palette.secondary.main,
-    height: "5rem",
-    width: "20rem",
+    marginBottom: "-4rem",
+  },
+  sendButton: {
+    textTransform: "none",
+  },
+  sendIcon: {
+    marginLeft: "2rem",
+  },
+  contactInfo: {
+    fontSize: "1.5rem",
+  },
+  contactIcon: {
+    height: "3rem",
+    width: "3rem",
+    margin: "1rem",
+  },
+  contactEmailIcon: {
+    height: "2.25rem",
+    width: "4rem",
+    margin: "1rem",
   },
 }))
 
@@ -50,43 +74,74 @@ const ContactPage = () => {
             alignItems="center"
             justifyContent="space-between"
           >
-            <Grid item classes={{ root: classes.titleContainer }}>
+            <Grid
+              item
+              classes={{
+                root: clsx(classes.titleContainer, classes.blockContainer),
+              }}
+            >
               <Typography variant="h4">Contact Us</Typography>
             </Grid>
-            <Grid item classes={{ root: classes.buttonContainer }}>
-              <Button>
+            <Grid
+              item
+              classes={{
+                root: clsx(classes.buttonContainer, classes.blockContainer),
+              }}
+            >
+              <Button classes={{ root: classes.sendButton }}>
                 <Typography variant="h4">send message</Typography>
-                <img src={send} alt="send" />
+                <img src={send} alt="send" className={classes.sendIcon} />
               </Button>
             </Grid>
           </Grid>
         </Grid>
         <Grid item>
           <Grid container direction="column">
-            <Grid item container>
+            <Grid item container alignItems="center">
               <Grid item>
-                <img src={address} alt="address" />
+                <img
+                  src={address}
+                  alt="address"
+                  className={classes.contactIcon}
+                />
               </Grid>
               <Grid item>
-                <Typography variant="h2">
+                <Typography
+                  variant="h2"
+                  classes={{ root: classes.contactInfo }}
+                >
                   1234 S Example St Wichita KS 67001
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item container>
+            <Grid item container alignItems="center">
               <Grid item>
-                <img src={phone} alt="phone" />
+                <img src={phone} alt="phone" className={classes.contactIcon} />
               </Grid>
               <Grid item>
-                <Typography variant="h2">(123) 456-7890</Typography>
+                <Typography
+                  variant="h2"
+                  classes={{ root: classes.contactInfo }}
+                >
+                  (123) 456-7890
+                </Typography>
               </Grid>
             </Grid>
-            <Grid item container>
+            <Grid item container alignItems="center">
               <Grid item>
-                <img src={email} alt="email" />
+                <img
+                  src={email}
+                  alt="email"
+                  className={classes.contactEmailIcon}
+                />
               </Grid>
               <Grid item>
-                <Typography variant="h2">contact@var-x.com.mx</Typography>
+                <Typography
+                  variant="h2"
+                  classes={{ root: classes.contactInfo }}
+                >
+                  contact@var-x.com.mx
+                </Typography>
               </Grid>
             </Grid>
           </Grid>

de4d0be54b8f7e7aba0bb1d67007b694ed8cf5e8
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-11-02 09:07:18 -0600

    94. Validacion del formulario

---
+++ frontend/src/components/ui/validate.js
@@ -0,0 +1,17 @@
+export default function validate(values) {
+  const vallidators = {
+    email: val => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.text(val),
+    phone: val =>
+      /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val),
+    name: val => val.length > 3,
+    message: val => val.length > 3,
+  }
+
+  const valid = {}
+
+  Object.keys(values).map(field => {
+    valid[field] = vallidators[field](values[field])
+  })
+
+  return valid
+}

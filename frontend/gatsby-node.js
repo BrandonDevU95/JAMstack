86d02b581d118727fadc820c8d359790a38336da
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-09 16:28:48 -0600

    104. Uso de consultas de página

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,7 +1,47 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const results = await graphql(`
+    {
+      product: allStrapiProduct {
+        edges {
+          node {
+            name
+            strapiId
+          }
+        }
+      }
+      categories: allStrapiCategory {
+        edges {
+          node {
+            strapiId
+            name
+            description
+          }
+        }
+      }
+    }
+  `)
 
-// You can delete this file if you're not using it
+  if (results.errors) {
+    throw results.errors
+  }
+
+  const products = results.data.products.edges
+  const categories = results.data.categories.edges
+
+  products.forEach(product => {
+    createPage({
+      path: `/product/${product.node.strapiId}`,
+      component: path.resolve(`./src/templates/product.js`),
+      context: {},
+    })
+  })
+
+  categories.forEach(category => {
+    createPage({
+      path: `/${category.node.name.toLowerCase()}`,
+      component: path.resolve(`./src/templates/category.js`),
+      context: {},
+    })
+  })
+}

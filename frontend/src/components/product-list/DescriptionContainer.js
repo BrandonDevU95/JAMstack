a1df6a89458172498d1d59683832e078fbbe946d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-20 17:26:13 -0600

    116. Agregar botón de diseño

--- frontend/src/components/product-list/DescriptionContainer.js
+++ frontend/src/components/product-list/DescriptionContainer.js
@@ -1,7 +1,9 @@
 import React from "react"
-import { Grid, Typography } from "@material-ui/core"
+import { Grid, Typography, ButtonGroup, Button } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import background from "../../images/toolbar-background.svg"
+import ListIcon from "../../images/List.js"
+import GridIcon from "../../images/Grid.js"
 
 const useStyles = makeStyles(theme => ({
   description: {
@@ -20,6 +22,11 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
+  },
+  button: {
+    border: `2px solid ${theme.palette.primary.main}`,
+    borderRightColor: `${theme.palette.primary.main} !important`,
+    borderRadius: 25,
   },
 }))
 
@@ -43,6 +50,16 @@ export default function DescriptionContainer({ name, description }) {
         >
           {description}
         </Typography>
+      </Grid>
+      <Grid item>
+        <ButtonGroup>
+          <Button classes={{ outlined: classes.button }}>
+            <ListIcon />
+          </Button>
+          <Button classes={{ outlined: classes.button }}>
+            <GridIcon />
+          </Button>
+        </ButtonGroup>
       </Grid>
     </Grid>
   )

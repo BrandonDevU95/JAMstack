2e1f82205e99cec399ae87653eeed6d28ea8a12f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-20 20:55:09 -0600

    126. Información de vista rápida

--- frontend/src/components/product-list/QuickView.js
+++ frontend/src/components/product-list/QuickView.js
@@ -1,7 +1,15 @@
 import React from "react"
-import { Grid, Typography, Dialog, DialogContent } from "@material-ui/core"
+import {
+  Grid,
+  Typography,
+  Dialog,
+  DialogContent,
+  Button,
+} from "@material-ui/core"
+import Rating from "../home/Rating"
 import { makeStyles } from "@material-ui/core/styles"
 import frame from "../../images/selected-frame.svg"
+import explore from "../../images/explore.svg"
 
 const useStyles = makeStyles(theme => ({
   selectedFrame: {
@@ -20,9 +28,34 @@ const useStyles = makeStyles(theme => ({
     width: "40rem",
     marginTop: "5rem",
   },
+  toolBar: {
+    backgroundColor: theme.palette.primary.main,
+    height: "13rem",
+    marginTop: "2rem",
+  },
+  stock: {
+    color: "#fff",
+  },
+  details: {
+    color: "#fff",
+    textTransform: "none",
+    fontSize: "1.5rem",
+  },
+  exploreIcon: {
+    height: "1.5rem",
+    width: "2rem",
+    marginLeft: "0.5rem",
+  },
+  detailButton: {
+    padding: "0",
+  },
+  infoContainer: {
+    height: "100%",
+    padding: "0.5rem 1rem",
+  },
 }))
 
-export default function QuickView({ open, setOpen, url }) {
+export default function QuickView({ open, setOpen, url, name }) {
   const classes = useStyles()
 
   return (
@@ -39,6 +72,39 @@ export default function QuickView({ open, setOpen, url }) {
               alt="Product Image"
               className={classes.productImage}
             />
+          </Grid>
+          <Grid item container classes={{ root: classes.toolBar }}>
+            <Grid item>
+              <Grid
+                container
+                direction="column"
+                justifyContent="space-between"
+                classes={{ root: classes.infoContainer }}
+              >
+                <Grid item>
+                  <Typography variant="h4">{name}</Typography>
+                  <Rating number={4} />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h3" classes={{ root: classes.stock }}>
+                    12 Currently In Stock
+                  </Typography>
+                  <Button classes={{ root: classes.detailButton }}>
+                    <Typography
+                      variant="h3"
+                      classes={{ root: classes.details }}
+                    >
+                      Details
+                    </Typography>
+                    <img
+                      src={explore}
+                      className={classes.exploreIcon}
+                      alt="go to product detail page"
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </DialogContent>

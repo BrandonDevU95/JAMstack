239c14cf0610ec23ee14fdc6bde4af203de42759
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-21 00:15:02 -0600

    131. Muestras

---
+++ frontend/src/components/product-list/Swatches.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Button, Grid, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+  swatch: {
+    border: "3px solid #fff",
+    height: "3rem",
+    width: "3rem",
+    minWidth: 0,
+    borderRadius: 50,
+  },
+  swatchesContainer: {
+    marginTop: "0.5rem",
+    "&:not(:first-child)": {
+      marginLeft: "-0.5rem",
+    },
+  },
+}))
+
+export default function Swatches({ colors }) {
+  const classes = useStyles()
+
+  return (
+    <Grid item container>
+      {colors.sort().map(color => (
+        <Grid item classes={{ root: classes.swatchesContainer }}>
+          <Button
+            style={{ backgroundColor: color }}
+            classes={{ root: classes.swatch }}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}

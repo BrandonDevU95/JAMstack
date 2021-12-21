a388de8209ad38bbd5a54d2c559426844016eff5
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-20 18:20:18 -0600

    120. Consulta de página de productos de categoría

--- frontend/src/templates/ProductList.js
+++ frontend/src/templates/ProductList.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Layout from "../components/ui/Layout"
 import { Grid } from "@material-ui/core"
+import { graphql } from "gatsby"
 import DynamicToobar from "../components/product-list/DynamicToobar"
 
 export default function ProductList({
   pageContext: { filterOptions, name, description },
+  data,
 }) {
+  console.log(data)
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
@@ -18,3 +21,26 @@ export default function ProductList({
     </Layout>
   )
 }
+
+export const query = graphql`
+  query GetCategoryProducts($id: String!) {
+    allStrapiProduct(filter: { category: { id: { eq: $id } } }) {
+      edges {
+        node {
+          strapiId
+          name
+          variants {
+            color
+            id
+            price
+            size
+            style
+            images {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
